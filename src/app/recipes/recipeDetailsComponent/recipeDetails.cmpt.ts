f2cc2ff6dd394ec39aaa1ad02fa2816a9e5fea6d
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2022-11-27 15:56:11 +0200

    Defined recipe types

--- src/app/recipes/recipeDetailsComponent/recipeDetails.cmpt.ts
+++ src/app/recipes/recipeDetailsComponent/recipeDetails.cmpt.ts
@@ -2,8 +2,22 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RecipeService } from '../recipes.service';
 
+type tCategory = { name: string; _id: number };
+
+type tComment = { comment: string; createdAt: Date; user: string; _id: number };
+
+type tLike = { like: boolean; user: string; _id: number };
+
 type tRecipe = {
+  category: tCategory;
+  comments: tComment[];
+  ingredients: { id: number; ingredient: string }[];
+  likes: tLike[];
+  methods: { id: number; method: string };
   name: string;
+  numberOfServings: number;
+  userId: { user: string };
+  _id: number;
 };
 
 @Component({
@@ -11,7 +25,7 @@ type tRecipe = {
   templateUrl: './recipeDetails.cmpt.html',
 })
 export class RecipeDetailsComponent {
-  recipe: tRecipe | any = {};
+  recipe!: tRecipe;
 
   constructor(
     private recipeService: RecipeService,
@@ -22,7 +36,7 @@ export class RecipeDetailsComponent {
     const id = this.activatedRoute.snapshot.params['id'];
     this.recipeService.getSingleRecipe(id).subscribe((recipe) => {
       this.recipe = recipe;
-      console.log(this.recipe.name);
+      console.log(this.recipe);
     });
   }
 }
